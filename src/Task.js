9f581da36093afcbae6fe622d85efa3953a1e4cc
Fajar Dwi Agustiar <fajardwagstr@gmail.com>
2022-12-20 09:22:03 +0700

    upload files

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Fajar Dwi Agustiar"; // TODO: replace this
+const studentId = "FE2576426"; // TODO: replace this
+
+export { url, studentName, studentId };
